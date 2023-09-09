acb452521f8aa8ad1efd9967cbb1519f0ef641c0
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-09-09 10:19:21 +0500

    desktop tugadi

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 AOS.init();
+
+let body = document.querySelector('body');
+let iconLove = document.querySelector('.header_icon_love');
+let mainModal = document.querySelector('.modal_main_wrapper');
+let closeBtn = mainModal.querySelector('.close_btn');
+
+if(iconLove && mainModal){
+    iconLove.onclick = () => {
+        mainModal.classList.remove('close')
+        mainModal.classList.add('open')
+        body.classList.add('bodyHidden')
+    } 
+
+    closeBtn.onclick = () => {
+        mainModal.classList.remove('open')
+        mainModal.classList.add('close')
+        body.classList.remove('bodyHidden')
+    }
+}
